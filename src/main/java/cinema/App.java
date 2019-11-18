6c34be3354dacc4720d4b99e303d42b949e63257
Romain <romain.gallard.lp4@gmail.com>
2019-11-18 14:56:35 +0100

    app.java

--- src/main/java/cinema/App.java
+++ src/main/java/cinema/App.java
@@ -11,65 +11,72 @@ import org.apache.commons.io.IOUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-
-
-
-
 public class App {
+
 	public int actorID;
-	
+
 	private static String IMDB_KEY = "0e6260469751f4d597183df49e4810b2";
 
 	public static String jsonFileName = "sample.json";
 
-	public static void film(String titre) throws MalformedURLException, IOException {
-		//String url = "https://api.themoviedb.org/3/movie/123?api_key="+IMDB_KEY;//id de recherche apres movie/
-		String url = "https://api.themoviedb.org/3/search/movie?api_key="+IMDB_KEY+"&query="+titre;
-		
+	public void film(String titre) throws MalformedURLException, IOException {
+		// String url = "https://api.themoviedb.org/3/movie/123?api_key="+IMDB_KEY;//id
+		// de recherche apres movie/
+		String url = "https://api.themoviedb.org/3/search/movie?api_key=" + IMDB_KEY + "&query=" + titre;
 
-		
 		String jsonText = IOUtils.toString(new URL(url), Charset.forName("UTF-8"));
 		writeJson(jsonText);
 
-		//JSONObject jsonComplet = new JSONObject(jsonText);
-		//JSONObject main = jsonComplet.getJSONObject("main");
+		// JSONObject jsonComplet = new JSONObject(jsonText);
+		// JSONObject main = jsonComplet.getJSONObject("main");
 
 	}
-	
-	
-	public static int acteur(String actor) throws MalformedURLException, IOException {
-		String urlActorID= "https://api.themoviedb.org/3/search/person?api_key="+IMDB_KEY+"&language=en-US&query="+actor+"&page=1&include_adult=false\r\n" + 
-				"";
-		
-		
+
+	public int acteur(String actor) throws MalformedURLException, IOException {
+		String urlActorID = "https://api.themoviedb.org/3/search/person?api_key=" + IMDB_KEY + "&language=en-US&query="
+				+ actor + "&page=1&include_adult=false\r\n" + "";
+
 		String jsonText = IOUtils.toString(new URL(urlActorID), Charset.forName("UTF-8"));
 		writeJson(jsonText);
 
 		JSONObject jsonComplet = new JSONObject(jsonText);
 		JSONArray main = jsonComplet.getJSONArray("results");
-		
-		
+
 		int actorID = (int) main.getJSONObject(0).get("id");
 		return actorID;
-		//System.out.println(actorID);
-		
+
+		// System.out.println(actorID);
+
 	}
-	public static void actorDetails (int actorID) {
-		String urlActorDetail = "https://api.themoviedb.org/3/person/"+actorID+"?api_key="+IMDB_KEY+"&language=en-US";
+
+	public void actorDetails(int actorID) throws MalformedURLException, IOException {
+		String urlActorDetail = "https://api.themoviedb.org/3/person/" + actorID + "?api_key=" + IMDB_KEY
+				+ "&language=en-US";
+
+		String jsonText = IOUtils.toString(new URL(urlActorDetail), Charset.forName("UTF-8"));
+		writeJson(jsonText);
+
+		JSONObject jsonComplet = new JSONObject(jsonText);
+		JSONObject main = jsonComplet.getJSONObject("1");
+		// String actorName = (String) main.getJSONObject(jsonText).get("name");
+		
+
+		String actorName = (String) main.getJSONObject(0).get("name");
+
 	}
-	
-	public static void actorCast  (int actorid) throws MalformedURLException, IOException {
-		String urlActorCast= "https://api.themoviedb.org/3/person/"+actorid+"/movie_credits?api_key=0e6260469751f4d597183df49e4810b2&language=en-US\r\n" + 
-				"";
-		
-		
+
+	public static void actorCast(int actorid) throws MalformedURLException, IOException {
+		String urlActorCast = "https://api.themoviedb.org/3/person/" + actorid
+				+ "/movie_credits?api_key=0e6260469751f4d597183df49e4810b2&language=en-US\r\n" + "";
+
 		String jsonText = IOUtils.toString(new URL(urlActorCast), Charset.forName("UTF-8"));
 		writeJson(jsonText);
 
-		//JSONObject jsonComplet = new JSONObject(jsonText);
-		//JSONObject main = jsonComplet.getJSONObject("main");
+		// JSONObject jsonComplet = new JSONObject(jsonText);
+		// JSONObject main = jsonComplet.getJSONObject("main");
 
 	}
+
 	public static void writeJson(String jsonText) {
 		BufferedWriter bw;
 
@@ -85,11 +92,13 @@ public class App {
 
 	}
 
-
 	public static void main(String[] args) throws MalformedURLException, IOException {
-		//film("Star wars");
-		acteur("tom cruise");
-		//System.out.println("l'id est "+actorID);
+		App test = new App();
+		// film("Star wars");
+		// test.acteur("tom cruise");
+		// System.out.println("l'id est "+actorID);
+		// System.out.println(test.acteur("Tom Cruise"));
+		test.actorDetails(test.acteur("tom cruise"));
 	}
 
 }
